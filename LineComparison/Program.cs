658a3a23d2c6c8e6fd2e18ba50919698ebcf6391
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-07 22:42:48 +0530

    commit message

---
+++ LineComparison/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace LineComparisonProgram
+{
+    internal class LIne_comparison
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to linecomparision problems");
+            Line_comparison line = new Line_comparison();
+            line.length_line();
+            Console.ReadLine();
+        }
+    }
+}
